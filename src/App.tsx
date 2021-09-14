555b1bd98fa56f62b6f9f513f557699e6660ba2f
DodyDharma440 <dharma.dody@yahoo.com>
2021-09-14 15:03:56 +0800

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <button className="px-4 py-2 bg-indigo-400">Tailwind</button>
+    </div>
+  );
+};
+
+export default App;
